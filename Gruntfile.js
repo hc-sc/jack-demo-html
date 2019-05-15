490cf6a78e29e2b769b8065ccf1df9f88b936776
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-15 09:47:51 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt)
+module.exports = function(grunt){
 
 grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
@@ -6,8 +6,8 @@ grunt.initConfig({
 			css: {
 				src: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/style.css',
 				dest: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/style.min.css'
-                }
-           }
+                },
+           },
        });
 
 
@@ -16,3 +16,4 @@ grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-html');
 grunt.loadNpmTasks('grunt-contrib-concat');
+}
