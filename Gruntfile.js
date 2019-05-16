716d2338731ed6f24a39c4d96750f8dc065560e3
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-16 11:02:35 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,8 @@ grunt.initConfig({
           quiet: false, // Optionally suppress output to standard out (defaults to false)
           clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
           clearCacheFilter: (key) => true, // Optionally defines which files should keep in cache
-          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+          noFail: false, // Optionally set to not fail on failed tests (will still fail on other errors)
+		run: true,
         },
         src: ['tests/tests.js']
       }
