f2ce5ac0023a57a4649bbada2f650ab83af4f0d4
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-16 10:24:09 -0400

    Update tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -3,7 +3,7 @@ describe('Array', function(){
   describe('#indexOf()', function(){
     if('should return -1 when the value is not present', function()){
       chai.assert.equal(-1, [1,2,3].indexOf(5));
-      chai.assert.equal(-1, [1,2,3].indexOf(0));
+      //chai.assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
 });
