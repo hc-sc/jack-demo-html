0fa38e6e7607a6354fe3a492cc36d4772a18497b
Mohamed Radwan <mohamed.radwan@canada.ca>
2019-05-16 08:44:34 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,17 @@ grunt.initConfig({
 		},	
 	},
 	    // Mocha
-    mochaTest: {
-      all: {
-        src: ['tests/testrunner.html'],
-      },
-      options: {
-        run: true
+     mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+          clearCacheFilter: (key) => true, // Optionally defines which files should keep in cache
+          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+        },
+        src: ['tests/tests.js']
       }
     }
 
