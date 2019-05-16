166dabcea3937b6f42d133cb0aaf85b0277777a1
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-16 11:18:38 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,6 @@ grunt.initConfig({
       test: {
         options: {
           reporter: 'Spec',
-		urls: ["http://majic-student.canadacentral.cloudapp.azure.com/"],
           captureFile: 'results.txt', // Optionally capture the reporter output to a file
           quiet: false, // Optionally suppress output to standard out (defaults to false)
           clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
