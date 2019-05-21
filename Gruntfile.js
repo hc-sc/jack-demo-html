1df3543a472dbbad60749f4ab344d69727fd7239
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-21 10:50:27 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,5 +44,5 @@ grunt.loadNpmTasks('grunt-mocha');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-html');
 grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-artifactory-artifact');
+grunt.loadNpmTasks('grunt-artifactory-deploy');
 }
