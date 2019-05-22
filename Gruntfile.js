eadef6db7bd79330b0abfab637638ca78545f7ad
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-22 13:39:33 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,4 +67,5 @@ module.exports = function(grunt) {
                     grunt.loadNpmTasks('grunt-html');
                     grunt.loadNpmTasks('grunt-contrib-uglify');
                     grunt.loadNpmTasks('grunt-artifactory-publish');
+                    grunt.loadNpmTasks('grunt-contrib-compress');
                 }
