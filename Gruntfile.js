f4399df57b856b1ce8def92a042554e9295dc4e9
Mohamed Radwan <mohamed.radwan@canada.ca>
2019-05-22 15:08:29 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,77 @@
-module.exports = function(grunt) {
+module.exports = function (grunt)
+{
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/package.json'),
-        cssmin: {
-            css: {
-                src: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/style.css',
-                dest: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/style.min.css'
-            },
-        },
+	grunt.initConfig(
+	{
+		pkg: grunt.file.readJSON('/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/package.json'),
+		cssmin:
+		{
+			css:
+			{
+				src: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/style.css',
+				dest: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/style.min.css'
+			},
+		},
 
-        htmllint: {
-            all: ['/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/index.html']
-        },
+		htmllint:
+		{
+			all: ['/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/index.html']
+		},
 
-        uglify: {
-            options: {
-                banner: "/*! app.min.js file */\n"
-            },
-            build: {
-                src: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/main.js',
-                dest: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/main.min.js'
-            },
-        },
-        mocha: {
-            test: {
-                options: {
-                    reporter: 'Spec',
-                    run: true,
-                    captureFile: 'results.txt',
-                    quiet: false,
-                    clearRequireCache: false,
-                    clearCacheFilter: (key) => true,
-                    noFail: false,
-                },
-                src: ['tests/tests.js'],
-            }
-        },
-        compress: {
-            main: {
-                options: {
-                    mode: 'gzip'
-                },
-                expand: true,
-                src: ['../ck_Builds_HelloWorld_HTML_master@tmp'],
-                dest: './home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/compress'
-            }
-        }
+		uglify:
+		{
+			options:
+			{
+				banner: "/*! app.min.js file */\n"
+			},
+			build:
+			{
+				src: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/main.js',
+				dest: '/home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master/main.min.js'
+			},
+		},
+		mocha:
+		{
+			test:
+			{
+				options:
+				{
+					reporter: 'Spec',
+					run: true,
+					captureFile: 'results.txt',
+					quiet: false,
+					clearRequireCache: false,
+					clearCacheFilter: (key) => true,
+					noFail: false,
+				},
+				src: ['tests/tests.js'],
+			}
+		},
 
-    });
-    grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-html');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-artifactory-publish');
+
+		compress:
+		{
+			main:
+			{
+				options:
+				{
+					archive: 'artifacts.zip'
+				},
+				files: [
+				{
+					expand: true,
+					cwd: 'path/',
+					src: ['../ck_Builds_HelloWorld_HTML_master@tmp'],
+					dest: './home/mradwan/jenkins/jenkins/workspace/ck_Builds_HelloWorld_HTML_master'
+				}]
+			}
+		},
+
+	});
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-mocha');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-html');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-artifactory-publish');
 }
