01ad808271d4f569f1edc0a1151a13e63eee591c
Mohamed Radwan <mohamed.radwan@canada.ca>
2019-05-23 14:38:11 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,8 +52,8 @@ module.exports = function(grunt) {
             options: {
                 apiKey: "AKCp5ccuuJiQjQvh2UxspNpYJ1R64TFGqjFgN9QVBiqoHx133zy4KoXR3hCXwV4hCmEHkU7zK",
                 repositoryPath: "https://build.scs-lab.com/artifactory/HelloWorld_HTML/",
-                targetPath: "artifacts",
-                packagePath: "../ck_Builds_HelloWorld_HTML_master/artifacts.zip"
+                targetPath: "artifacts.tgz",
+                packagePath: "../ck_Builds_HelloWorld_HTML_master@tmp/artifactory"
             }
         }
     });
