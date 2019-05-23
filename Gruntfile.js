f8a1ef563ac86155d79f75bf760db96053661656
Bitopi Bhowmik <bitopi.bhowmik@canada.ca>
2019-05-23 09:45:30 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,8 +53,8 @@ module.exports = function(grunt) {
             options: {
                 apiKey: "AKCp5ccuuJiQjQvh2UxspNpYJ1R64TFGqjFgN9QVBiqoHx133zy4KoXR3hCXwV4hCmEHkU7zK",
                 repositoryPath: "https://build.scs-lab.com/artifactory/HelloWorld_HTML/",
-                targetPath: "artifacts.tgz",
-                packagePath: "../ck_Builds_HelloWorld_HTML_master@tmp"         
+                targetPath: "artifacts",
+                packagePath: "../ck_Builds_HelloWorld_HTML_master/artifacts.tgz"         
             }
         }
     });
