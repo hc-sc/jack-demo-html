5a06fecb87d9e1c3752cfd8149592a12091a3447
Mohamed Radwan <mohamed.radwan@canada.ca>
2019-05-23 14:41:46 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,10 +50,10 @@ module.exports = function(grunt) {
         },
         artdeploy: {
             options: {
-                apiKey: "AKCp5ccuuJiQjQvh2UxspNpYJ1R64TFGqjFgN9QVBiqoHx133zy4KoXR3hCXwV4hCmEHkU7zK",
-                repositoryPath: "https://build.scs-lab.com/artifactory/HelloWorld_HTML/",
-                targetPath: "artifacts.tgz",
-                packagePath: "../ck_Builds_HelloWorld_HTML_master@tmp/artifactory.tgz"
+                apiKey: 'AKCp5ccuuJiQjQvh2UxspNpYJ1R64TFGqjFgN9QVBiqoHx133zy4KoXR3hCXwV4hCmEHkU7zK',
+                repositoryPath: 'https://build.scs-lab.com/artifactory/HelloWorld_HTML/',
+                targetPath: 'artifacts.tgz',
+                packagePath: '../ck_Builds_HelloWorld_HTML_master/artifactory'
             }
         }
     });
