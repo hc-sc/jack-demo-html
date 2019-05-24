25c772b896359fe72768551f43fcbd86704fcf98
Mohamed Radwan <mohamed.radwan@canada.ca>
2019-05-24 09:15:33 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,6 +81,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-html');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-   // grunt.loadNpmTasks('grunt-artifactory-deploy');
+    grunt.loadNpmTasks('grunt-artifactory-deploy');
     grunt.loadNpmTasks('grunt-artifactory-publish');
 }
