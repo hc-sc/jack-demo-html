fdc2d408ff0109c560f1c164efa8a845641a92f4
Mradwan <mohamed.radwan@canada.ca>
2019-06-13 15:47:33 -0400

    modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,12 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      }
-    ]
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
+    new HtmlWebpackPlugin()
   ]
-};
+}
