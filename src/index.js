3890056c1d3bb6e31b0908618b07f96f86fe9f13
Mradwan <mohamed.radwan@canada.ca>
2019-06-14 13:06:47 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-/* import 'assets/calc/calc.scss';
- */
-import {calc}
-=======
-import './assets/calc/calc.scss';
->>>>>>> 05a6f47c7da04f5b6294209d9b2498137bac0de4
-
 console.log('Webpack Zero Config Works!');
