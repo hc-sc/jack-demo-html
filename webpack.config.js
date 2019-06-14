b2227c56f52514c0c0e10a73dca02f56c418eb95
Mradwan <mohamed.radwan@canada.ca>
2019-06-14 09:50:07 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: ['./src/index.js','assets/calc/calc.js', 'assets/wet-boew/js/wet-boew.min.js', 'assets/GCWeb/js/theme.min.js'],
   output: {
     path: __dirname + '/dist',
     filename: 'index_bundle.js'
