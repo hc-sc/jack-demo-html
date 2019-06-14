159e0a50f7ed5e8f9c7624bae3cc1ee27a97a6b4
bitopibhowmik <51677787+bitopibhowmik@users.noreply.github.com>
2019-06-14 09:59:10 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
         rules: [{
             test: /\.scss$/,
             use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
                 "sass-loader" // compiles Sass to CSS, using Node Sass by default
             ]
         }]
