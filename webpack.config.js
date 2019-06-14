2b5975c7542816645a9ed7878f111d1e25de4973
bitopibhowmik <51677787+bitopibhowmik@users.noreply.github.com>
2019-06-14 10:13:44 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
      rules:[
              {
                  test:/\.scss$/,
-                 use:['style-loader','css-loader', 'sass-loader']
+                 use:['sass-loader']
               }
       ]
    },
