4e2e6697a1d08ba5f56b7416d6458d12c5cfe03c
Mradwan <mohamed.radwan@canada.ca>
2019-06-14 13:03:42 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     rules:[
       {
         test: /\.html$/,
-        use [
+        use: [
           {
              loader : "html-loader",
              options : {minimize: true}
