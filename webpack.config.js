800e8f9a98a1c2bac472599901d1813a0729d6cf
bitopibhowmik <51677787+bitopibhowmik@users.noreply.github.com>
2019-06-14 09:57:47 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,17 @@ module.exports = {
     path: __dirname + '/public',
     filename: 'index_bundle.js'
   },
+  module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
   plugins: [
     new HtmlWebpackPlugin()
   ]
-}
+};
