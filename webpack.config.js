cceef6d90ef7eac4323bca6b153dd6371eb9c559
Mradwan <mohamed.radwan@canada.ca>
2019-06-17 11:07:27 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -5,43 +5,31 @@ module.exports = {
     './src/assets/GCWeb/js/theme.min.js',
     './src/assets/jquery.js'
   ],
-<<<<<<< HEAD
  output: {
    filename: "bundle.js"
  },
  module: {
+   preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+
+      }
+   ],
    loaders: [
      {
        test: /\.es6$/,
        exclude: /node_modules/,
        loader: 'babel-loader',
        query: {
-         presets: ['react', 'es2015']
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
        }
-     }
+      }
    ]
  },
  resolve: {
    extensions: ['', '.js', '.es6']
- },
-}
-=======
-	mode:'production',
-  output: {
-    path: __dirname,
-    publicPath: '/src',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+ }
 };
->>>>>>> 56aee6183a1a534d2a3bb0fe3424272cc3f64ef5
