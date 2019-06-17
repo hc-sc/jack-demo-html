e1ee9322b711b611ea9c90e2d9f370a7f4126bc3
bitopibhowmik <51677787+bitopibhowmik@users.noreply.github.com>
2019-06-17 10:28:26 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "script-loader"
+          loader: "raw-loader"
         }
       }
     ]
