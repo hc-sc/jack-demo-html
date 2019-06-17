da8de5909f9f344d7a75a04758db352d1a867aad
Mradwan <mohamed.radwan@canada.ca>
2019-06-17 09:03:51 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,28 @@
+var webpack = require('webpack')
 module.exports = {
-  entry: [
+    entry: {
+        entry: __dirname + [
     './src/assets/calc/calc.js',
     './src/assets/wet-boew/js/wet-boew.min.js',
     './src/assets/GCWeb/js/theme.min.js'
   ],
-	mode:'production',
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "script-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
+    },
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
             }
-          }
         ]
-      }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
     ]
-  }
-};
+}
