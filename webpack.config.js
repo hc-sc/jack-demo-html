4cd0043a6ce61c7ddac27b751240ccdb060c87e1
Mradwan <mohamed.radwan@canada.ca>
2019-06-17 10:55:13 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  entry: [
+ entry: [
     './src/assets/calc/calc.js',
     './src/assets/wet-boew/js/wet-boew.min.js',
     './src/assets/GCWeb/js/theme.min.js',
     './src/assets/jquery.js'
   ],
-	mode:'production',
-  output: {
-    path: __dirname,
-    publicPath: '/src',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "raw-loader"
-        }
-      }
-    ]
-  }
-};
+ output: {
+   filename: "bundle.js"
+ },
+ module: {
+   loaders: [
+     {
+       test: /\.es6$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015']
+       }
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.es6']
+ },
+}
