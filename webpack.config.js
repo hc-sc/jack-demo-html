46866fdf2c95b5a950882132df1821fd48f7f789
Mradwan <mohamed.radwan@canada.ca>
2019-06-17 12:59:10 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,40 @@
 module.exports = {
-  entry: 'app.js',
+  entry: [
+    'app.js'
+  ],
+	mode:'production',
   output: {
-    path: __dirname + '/src',
+    path: __dirname,
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      loaders: [
-        'style-loader',
-        'css-loader'
-      ]
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "script-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
   }
-}
+};
