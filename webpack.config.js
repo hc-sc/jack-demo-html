38760e61ff3bc2affa53b944fbb43b616f544509
Mradwan <mohamed.radwan@canada.ca>
2019-06-17 09:33:56 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 module.exports = {
   entry: [
     './src/assets/calc/calc.js',
-    './src/assets/wet-boew/js/wet-boew.min.js',
-    './src/assets/GCWeb/js/theme.min.js'
+    './src/assets/wet-boew/js/wet-boew.js',
+    './src/assets/GCWeb/js/theme.js'
   ],
-  output:{
+	mode:'production',
+  output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    new MinifyPlugin(minifyOpts, pluginOpts)
-  ]
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "script-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
