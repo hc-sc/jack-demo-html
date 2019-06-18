095803827582fcb0b11a0955eed11b27af68fef7
Mradwan <mohamed.radwan@canada.ca>
2019-06-18 16:28:01 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const mode = process.env.NODE_ENV || "development";
 
 const entry = {
-  "calc": "./src/assets/calc/calc.js"
-
+  "calc": ["./src/assets/calc/calc.js", "./src/assets/calc/calc.scss"]
 }
 
 const output = {
@@ -10,6 +9,16 @@ const output = {
   path: __dirname+ "/dist"
 }
 
+const _module = {
+   rules: [{
+      test: /\.scss$/,
+      use: [
+         "style-loader", // creates style nodes from JS strings
+         "css-loader", // translates CSS into CommonJS
+         "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    }]
+}
 
 module.exports = {
   entry,
