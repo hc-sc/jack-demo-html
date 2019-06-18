7ed538741f78cc9a7e8d3c16c4006d0e157738b1
Mradwan <mohamed.radwan@canada.ca>
2019-06-18 16:41:57 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const mode = process.env.NODE_ENV || "development";
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const entry = {
   "calc": ["./src/assets/calc/calc.js", "./src/assets/calc/calc.scss"]
 }
 
 const output = {
-  filename: "[name].js",
+  filename: "[name].new.js",
   path: __dirname+ "/dist"
 }
 
@@ -13,16 +14,24 @@ const _module = {
    rules: [{
       test: /\.scss$/,
       use: [
-         "style-loader", // creates style nodes from JS strings
+        MiniCssExtractPlugin.loader,
+        "style-loader", // creates style nodes from JS strings
          "css-loader", // translates CSS into CommonJS
          "sass-loader" // compiles Sass to CSS, using Node Sass by default
         ]
     }]
 }
 
+const plugins = [
+  new MiniCssExtractPlugin({
+      filename: "[name].new.css"
+    })
+]
+
 module.exports = {
   entry,
   mode,
   module: _module,
-  output
+  output,
+  plugins
 }
