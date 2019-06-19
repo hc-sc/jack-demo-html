344b7dac35efd5b9a2f36c14760ea60688839222
Mradwan <mohamed.radwan@canada.ca>
2019-06-19 16:23:43 -0400

    configuring webpack

--- src/test/calc.test.js
+++ src/test/calc.test.js
@@ -3,23 +3,16 @@ var calc = require('../assets/calc/calc.js');
 
 describe('Calculator Tests', function() {
 
-	it('returns 1+1=2', function(done) {
+	it('Calculating 2 numbers', function(done) {
 		assert.equal(calc.calculate("1+1"), 2);
-		done();
-		});
-	it('returns 6-4=2', function(done) {
-		assert.equal(calc.calculate("6-4"), 2);
-		done();
-		});
-	it('returns 2*2=4', function(done) {
-		assert.equal(calc.calculate("2*2"), 4);
-		done();
-		});
-	it('returns 4/2=2', function(done) {
-		assert.equal(calc.calculate("4/2"), 2);
+    assert.equal(calc.calculate("6-4"), 2);
+    assert.equal(calc.calculate("2*2"), 4);
+    assert.equal(calc.calculate("4/2"), 2);
+    assert.equal(calc.calculate("-2+2"), 0);
 		done();
 		});
 });
+
 
 describe('Error Tests', function() {
   it('returns error for large numbers (999999999999*999999)', function(done){
