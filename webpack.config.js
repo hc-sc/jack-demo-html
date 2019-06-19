ea407c3d24e1253219af6cb2a2a2dd834fbd06be
Mradwan <mohamed.radwan@canada.ca>
2019-06-19 15:17:18 -0400

    configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,52 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const entry = {
-    "calc": ["./src/assets/calc/calc.js", "./src/assets/calc/calc.scss"]
+	"calc": ["./src/assets/calc/calc.js", "./src/assets/calc/calc.scss"]
 }
 
 const optimization = {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
 }
 
 const output = {
-    filename: "[name].min.js",
-    path: __dirname + "/dist"
+	filename: "[name].min.js",
+	path: __dirname + "/dist"
 }
 
 const _module = {
-    rules: [{
-            test: /\.s?css$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'sass-loader'
-            ]
-        },
-        {
-            test: /(htm|html|xhtml|hbs|handlebars|php|ejs)$/,
-            loader: 'htmllint-loader',
-            exclude: /(node_modules)/,
-        },
+	rules: [{
+			test: /\.s?css$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				'css-loader',
+				'sass-loader'
+			]
+		},
+/* 		{
+			test: /(htm|html|xhtml|hbs|handlebars|php|ejs)$/,
+			loader: 'htmllint-loader',
+			exclude: /(node_modules)/,
+		}, */
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "eslint-loader",
+		}
 
-    ]
+	]
 }
 
 const plugins = [
-    new MiniCssExtractPlugin({
-        filename: "calc.min.css"
-    })
+	new MiniCssExtractPlugin({
+		filename: "calc.min.css"
+	})
 ]
 
 module.exports = {
-    entry,
-    mode: 'production',
-    module: _module,
-    output,
-    plugins,
-    optimization
+	entry,
+	mode: 'production',
+	module: _module,
+	output,
+	plugins,
+	optimization
 }
