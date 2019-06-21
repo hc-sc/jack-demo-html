91e51b52d72c9bdc24dddb3e8f7f05e3f2a19811
Mradwan <mohamed.radwan@canada.ca>
2019-06-21 12:34:03 -0400

    test

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,26 +16,8 @@ const optimization = {
 		new TerserJSPlugin(),
 		new OptimizeCSSAssetsPlugin()
 	],
-	splitChunks: {
-      chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
+    splitChunks: {
+      chunks: 'all'
     }
 }
 
