e7c4e0af7cb61b86f321524e91790d8d44ee6dd1
MohamedRadwan <mohamedhisham13@gmail.com>
2019-06-25 09:00:08 -0400

    test

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,16 +24,12 @@ const optimization = {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
             return `npm.${packageName.replace('@', '')}`;
           },
         },
       },
-	}
+	},
 	minimizer: [
 		new TerserJSPlugin(),
 		new OptimizeCSSAssetsPlugin()
