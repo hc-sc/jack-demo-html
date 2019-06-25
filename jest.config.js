6615411c25e14f2d9388256cea91ae56ec8b66d3
MohamedRadwan <mohamedhisham13@gmail.com>
2019-06-25 13:02:17 -0400

    unit test config

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,6 @@ module.exports = {
       "displayName": "unit test",
 	"testEnvironment": "jsdom",
 	"testRegex":["/src/test/unitTest"]
-    },
-    {
-      "displayName": "ui test",
-      "preset": "jest-puppeteer",
-      "testRegex": ["/src/test/uiTest/"]
-
     }
-]   
 
- 
-};
+]};
