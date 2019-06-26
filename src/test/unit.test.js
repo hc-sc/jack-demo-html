1beb62be1dcd5ea94febe3a36a584e8a947f7a3d
MohamedRadwan <mohamedhisham13@gmail.com>
2019-06-26 14:06:57 -0400

    unit testing updated

--- src/test/unit.test.js
+++ src/test/unit.test.js
@@ -9,91 +9,91 @@ const {
 
 describe('Testing calculator functions', () => {	
   test('2 number calculation', () => {
-		expect(calculate("1+1").toEqual(2);
-		expect(calculate("6-4").toEqual(2);
-		expect(calculate("2*2").toEqual(4);
-		expect(calculate("4/2").toEqual(2);
-		expect(calculate("-2+2").toEqual(0);
+		expect(calculate("1+1").toEqual(2)
+		expect(calculate("6-4").toEqual(2)
+		expect(calculate("2*2").toEqual(4)
+		expect(calculate("4/2").toEqual(2)
+		expect(calculate("-2+2").toEqual(0)
   })    
   test('decimal calculation', () => {
-		expect(calculate("1.9+1.1").toEqual(3);
-		expect(calculate("6.7-4.2").toEqual(2.5);
-		expect(calculate("2.1*2.8").toEqual(5.88);
-		expect(calculate("4.6/2.3").toEqual(2);
-		expect(calculate("-2+2.2").toEqual(0.2);
+		expect(calculate("1.9+1.1").toEqual(3)
+		expect(calculate("6.7-4.2").toEqual(2.5)
+		expect(calculate("2.1*2.8").toEqual(5.88)
+		expect(calculate("4.6/2.3").toEqual(2)
+		expect(calculate("-2+2.2").toEqual(0.2)
   })    
 })
 
 
 describe('clear one element', () => {	
   test('clear one number', () => {
-		expect(clearCE(6, "123456").toEqual("12345");
+		expect(clearCE(6, "123456").toEqual("12345")
   })    
   test('clearing one out of one elements', () => {
-		expect(clearCE(1, "9").toEqual("0");
+		expect(clearCE(1, "9").toEqual("0")
   })    
   test('clearing an operator', () => {
-		expect(clearCE(2, "9+").toEqual("9");
+		expect(clearCE(2, "9+").toEqual("9")
   })  
 })
 
 describe('clear All', () => {	
   test('clearing all elements', () => {
-		expect(clearAC("123456").toEqual("0");
-		expect(clearAC("9").toEqual("0");
-		expect(clearAC("0").toEqual("0");
+		expect(clearAC("123456").toEqual("0")
+		expect(clearAC("9").toEqual("0")
+		expect(clearAC("0").toEqual("0")
   })  
 })
 
 describe('Checking for error thrown by the calculate function', () => {	
   test('returns error for number larger than 8', () => {
-		expect(calculate("999999999999*999999").toEqual("Lrg");
+		expect(calculate("999999999999*999999").toEqual("Lrg")
   })  
   
   test('returns error for missing operator (90*)', () => {
-		expect(calculate("90*").toEqual("Err");
+		expect(calculate("90*").toEqual("Err")
   })  
   
   test('eturns error for letter in equation', () => {	
-		expect(calculate("x+4").toEqual("Err");
+		expect(calculate("x+4").toEqual("Err")
   })
 })
 
 describe('Calculator function', () => {
   test('check return after entering a number', () => {
-		expect(calculator("1",0,"")).toEqual("1");
-		expect(calculator("2",0,"")).toEqual("2");
-		expect(calculator("3",0,"")).toEqual("3");
-		expect(calculator("4",0,"")).toEqual("4");
-		expect(calculator("5",0,"")).toEqual("5");
-		expect(calculator("6",0,"")).toEqual("6");
-		expect(calculator("7",0,"")).toEqual("7");
-		expect(calculator("8",0,"")).toEqual("8");
-		expect(calculator("9",0,"")).toEqual("9");
-		expect(calculator(".",0,"")).toEqual(".");
-		expect(calculator("-",0,"")).toEqual("-");
-		expect(calculator("+",0,"")).toEqual("+");		
-		expect(calculator("AC",3,"123")).toEqual("0");
-		expect(calculator("CE",3,"123")).toEqual("12");
-		expect(calculator("CE",1,"1")).toEqual("0");
+		expect(calculator("1",0,"")).toEqual("1")
+		expect(calculator("2",0,"")).toEqual("2")
+		expect(calculator("3",0,"")).toEqual("3")
+		expect(calculator("4",0,"")).toEqual("4")
+		expect(calculator("5",0,"")).toEqual("5")
+		expect(calculator("6",0,"")).toEqual("6")
+		expect(calculator("7",0,"")).toEqual("7")
+		expect(calculator("8",0,"")).toEqual("8")
+		expect(calculator("9",0,"")).toEqual("9")
+		expect(calculator(".",0,"")).toEqual(".")
+		expect(calculator("-",0,"")).toEqual("-")
+		expect(calculator("+",0,"")).toEqual("+")		
+		expect(calculator("AC",3,"123")).toEqual("0")
+		expect(calculator("CE",3,"123")).toEqual("12")
+		expect(calculator("CE",1,"1")).toEqual("0")
   })  
   
   test('Checking if the calculation is preformed', () => {
-		expect(calculator("=",3,"1+1")).toEqual("2");
-		expect(calculator("=",3,"6-4")).toEqual("2");
-		expect(calculator("=",3,"2*2")).toEqual("4");
-		expect(calculator("=",3,"4/2")).toEqual("2");
-		expect(calculator("=",3,"-2+2")).toEqual("0");
-		expect(calculator("=",2,".2")).toEqual("0.2");
-		expect(calculator("=",3,"2.0")).toEqual("2");
+		expect(calculator("=",3,"1+1")).toEqual("2")
+		expect(calculator("=",3,"6-4")).toEqual("2")
+		expect(calculator("=",3,"2*2")).toEqual("4")
+		expect(calculator("=",3,"4/2")).toEqual("2")
+		expect(calculator("=",3,"-2+2")).toEqual("0")
+		expect(calculator("=",2,".2")).toEqual("0.2")
+		expect(calculator("=",3,"2.0")).toEqual("2")
   })  
   
   test('checking for errors', () => {	
-		expect(calculator("=",4,"1..2")).toEqual("Err");
-		expect(calculator("=",5,"1.2.3")).toEqual("Err");		
-		expect(calculator("=",3,"..2")).toEqual("Err");
-		expect(calculator("=",5,"..2+2")).toEqual("Err");
-		expect(calculator("=",9,"9*9999999")).toEqual("Lrg");
+		expect(calculator("=",4,"1..2")).toEqual("Err")
+		expect(calculator("=",5,"1.2.3")).toEqual("Err")
+		expect(calculator("=",3,"..2")).toEqual("Err")
+		expect(calculator("=",5,"..2+2")).toEqual("Err")
+		expect(calculator("=",9,"9*9999999")).toEqual("Lrg")
   })
 })
 
