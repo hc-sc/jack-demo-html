2e7ebf7bf1bdd77f277e7830218c5a1ab4279753
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-06-26 13:28:17 +0000

    add ui tests

--- src/test/ui.test.js
+++ src/test/ui.test.js
@@ -3,12 +3,18 @@ describe('check title', () => {
     await page.goto('https://wet-boew.github.io/themes-dist/GCWeb/index-en.html')
   })
 
-  it('should display "Index of /" text on page', async () => {
+  test('should display "Canada.ca theme" text on page', async () => {
     await expect(page).toMatch('Canada.ca theme')
   })
   test("should assert that a div named landscape exists", async () => {
     const landscape = await page.$eval(".landscape", el => (el ? true : false));
     expect(landscape).toBe(true);
   });
- 
+  test("french link should lead to french site", async () => {
+    const element = await page.$('[lang="fr"]');
+    await page.click('a[lang=fr]');
+    const site = await page.url();
+expect(site).toBe('https://wet-boew.github.io/themes-dist/GCWeb/index-fr.html');
+  });
+
 })
