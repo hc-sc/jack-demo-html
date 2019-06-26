2b90c77fcc8cf767547af83d8060a399ec39ff57
MohamedRadwan <mohamedhisham13@gmail.com>
2019-06-26 15:34:18 -0400

    unit testing updated

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,6 @@ module.exports = {
       "testRegex":["/src/test/unit.test.js"]
 
 
-    },
-    {	
-      "displayName": "ui test",
-      "preset": "jest-puppeteer",
-      "testRegex": ["/src/test/ui.test.js"]
-
     }
 ]   
 
