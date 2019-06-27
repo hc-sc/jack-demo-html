70ec0510fc051b32619d22037a6567e1c4d3c41f
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-06-27 17:30:39 +0000

    ui test update

--- src/test/ui.test.js
+++ src/test/ui.test.js
@@ -1,7 +1,7 @@
-describe('UI Tests', () => {
+describe('General Tests', () => {
   beforeAll(async () => {
     await page.goto('file://localhost/home/bbhowmik/jack-demo-html/dist/index-en.html')
-  })
+  });
 
   test('should display "Date Modified" text on page', async () => {
     await expect(page).toMatch('Date modified: 2019-06-04')
@@ -10,11 +10,20 @@ describe('UI Tests', () => {
     const landscape = await page.$eval(".landscape", el => (el ? true : false));
     expect(landscape).toBe(true);
   });
-  test("clicking button 9 should display 9", async () => {
+      
+})
+
+describe('Calculator Tests', () => {
+ test("clicking button 9 should display 9", async () => {
     await page.click('button[id="nine"]');
-  	 const element = await page.$(".window");
-    	 const text = await (await element.getProperty('textContent')).jsonValue();
-	 expect(text).toBe('9');
+         const element = await page.$(".window");
+         const text = await (await element.getProperty('textContent')).jsonValue();
+         expect(text).toBe('9');
   });
-     
+  test("clicking button + should display 9+", async () => {
+    await page.click('button[id="add"]');
+         const element = await page.$(".window");
+         const text = await (await element.getProperty('textContent')).jsonValue();
+         expect(text).toBe('9+');
+  });
 })
