dad3fa8d90636979b04f0e46d8258f76ac2c263b
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-06-27 14:12:18 +0000

    update ui test

--- src/test/ui.test.js
+++ src/test/ui.test.js
@@ -1,19 +1,20 @@
 describe('UI Tests', () => {
   beforeAll(async () => {
-    await page.goto('https://wet-boew.github.io/themes-dist/GCWeb/index-en.html')
+    await page.goto('file://localhost/home/bbhowmik/jack-demo-html/dist/index-en.html')
   })
 
-  test('should display "Canada.ca theme" text on page', async () => {
-    await expect(page).toMatch('Canada.ca theme')
+  test('should display "" text on page', async () => {
+    await expect(page).toMatch('Date modified: 2019-06-04')
   });
   test("should assert that a div named landscape exists", async () => {
     const landscape = await page.$eval(".landscape", el => (el ? true : false));
     expect(landscape).toBe(true);
   });
   test("french link should lead to french site", async () => {
-    await page.click('a[lang=fr]');
-    const site = await page.url();
-expect(site).toBe('https://wet-boew.github.io/themes-dist/GCWeb/index-fr.html');
+    await page.click('button[id="nine"]');
+  	 const element = await page.$(".window");
+    	 const text = await (await element.getProperty('textContent')).jsonValue();
+	 expect(text).toBe('9');
   });
      
 })
