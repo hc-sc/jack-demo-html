84be4ae0862c02db1127fc715790ad7621f3f897
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-06-27 14:14:30 +0000

    update ui test

--- src/test/ui.test.js
+++ src/test/ui.test.js
@@ -3,14 +3,14 @@ describe('UI Tests', () => {
     await page.goto('file://localhost/home/bbhowmik/jack-demo-html/dist/index-en.html')
   })
 
-  test('should display "" text on page', async () => {
+  test('should display "Date Modified" text on page', async () => {
     await expect(page).toMatch('Date modified: 2019-06-04')
   });
   test("should assert that a div named landscape exists", async () => {
     const landscape = await page.$eval(".landscape", el => (el ? true : false));
     expect(landscape).toBe(true);
   });
-  test("french link should lead to french site", async () => {
+  test("clicking button 9 should display 9", async () => {
     await page.click('button[id="nine"]');
   	 const element = await page.$(".window");
     	 const text = await (await element.getProperty('textContent')).jsonValue();
