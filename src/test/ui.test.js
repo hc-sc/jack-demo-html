d4f25468deb4a3eba1619215331ba1986bc82ed0
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-06-28 15:05:09 +0000

    update ui

--- src/test/ui.test.js
+++ src/test/ui.test.js
@@ -10,7 +10,11 @@ describe('General Tests', () => {
     const landscape = await page.$eval(".landscape", el => (el ? true : false));
     expect(landscape).toBe(true);
   });
-      
+  test("should select the 'something is broken' checkbox", async () => {
+    await page.click('summary[class="btn btn-default text-center"]');
+    await page.click('input[id="problem1"]');
+    // expect(landscape).toBe(true);
+  });
 })
 
 describe('Calculator Tests', () => {
@@ -27,8 +31,8 @@ describe('Calculator Tests', () => {
          expect(text).toBe('9+');
   });
  test("inputting 9+5= should display 14", async () => {
-	await page.click('button[id="five"]');
-	await page.click('button[id="ans"]');
+        await page.click('button[id="five"]');
+        await page.click('button[id="ans"]');
          const element = await page.$(".window");
          const text = await (await element.getProperty('textContent')).jsonValue();
          expect(text).toBe('14');
@@ -42,14 +46,29 @@ test("clicking AC should display 0", async () => {
  test("inputting 3*125= should display 375", async () => {
         await page.click('button[id="three"]');
         await page.click('button[id="multiply"]');
- 	await page.click('button[id="one"]');
-	await page.click('button[id="two"]');
-	await page.click('button[id="five"]');
- 	await page.click('button[id="ans"]');
+        await page.click('button[id="one"]');
+        await page.click('button[id="two"]');
+        await page.click('button[id="five"]');
+        await page.click('button[id="ans"]');
          const element = await page.$(".window");
          const text = await (await element.getProperty('textContent')).jsonValue();
          expect(text).toBe('375');
   });
+ test("inputting 4.5-1.7= should display 2.8", async () => {
+        await page.click('button[id="AC"]');
+	await page.click('button[id="four"]');
+        await page.click('button[id="point"]');
+        await page.click('button[id="five"]');
+        await page.click('button[id="subtract"]');
+        await page.click('button[id="one"]');
+	await page.click('button[id="point"]');
+	await page.click('button[id="seven"]');
+        await page.click('button[id="ans"]');
+         const element = await page.$(".window");
+         const text = await (await element.getProperty('textContent')).jsonValue();
+         expect(text).toBe('2.8');
+  });
 
 
 })
+
