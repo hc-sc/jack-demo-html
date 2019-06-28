4da06e6897e3e02fb2761f5e7081204aa34386bd
MohamedRadwan <mohamedhisham13@gmail.com>
2019-06-28 09:21:58 -0400

    Breaking thre unit test for faster testing

--- src/test/unit-4.test.js
+++ src/test/unit-4.test.js
@@ -1,4 +1,7 @@
-const calculator = require('../calc-assets/calc.js');
+const {
+	calculator
+} = require('../calc-assets/calc.js');
+
 describe('Calculator function', () => {
 	test('check return after entering a number', () => {
 		expect(calculator('1', 0, '')).toBe('1')
