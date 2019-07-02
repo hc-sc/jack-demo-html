353390af7295f133708e98357ef713837f8736aa
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-07-02 18:42:54 +0000

    add sandbox

---
+++ sandbox.sh
@@ -0,0 +1,5 @@
+cd node_modules/puppeteer/.local-chromium/linux-672088/chrome-linux/
+sudo chown root:root chrome_sandbox
+sudo chmod 4755 chrome_sandbox
+sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
