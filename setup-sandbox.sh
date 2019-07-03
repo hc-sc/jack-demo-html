ff5ceafc66b1096ef64b4a7bff4fc78f33fb239c
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-03 15:13:06 -0400

    Update setup-sandbox.sh

--- setup-sandbox.sh
+++ setup-sandbox.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# This script setsup the sandbox for the UI testing. This script will go back to the original dir after the setup is done
-current_dir=$PWD
-echo "Setup script started will resume to ${current_dir} after completion"
+
 cd node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/
 sudo chown root:root chrome_sandbox
 sudo chmod 4755 chrome_sandbox
 sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
-echo "Setup is completed"
-cd $current_dir
