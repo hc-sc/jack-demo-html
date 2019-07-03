5499628ba314003c0fe6d84aa5b0a20afd336cde
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-03 13:52:16 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,7 @@ module.exports = {
           '/src/test/unit-1.test.js',
           '/src/test/unit-2.test.js',
           '/src/test/unit-3.test.js',
-          '/src/test/unit-4.test.js'
-          
+          '/src/test/unit-4.test.js'     
         ]
-    }
-    
+    }    
 ]};
