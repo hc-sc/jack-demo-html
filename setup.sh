01be276fafb6c1ec447ed5e95a225ffbae7472ba
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-03 13:49:25 -0400

    Setup script for npm and the UI environment
    - This scripts run npm install and then sets up the UI sandbox and returns to home dir

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script setsup the sandbox for the UI testing. This script will go back to the original dir after the setup is done
+#Needs to be excuted everytime jenkins runs a new build via > . sandbox-setup.sh
+# please note using ./ sandbox-setup.sh will not setup the sandbox since it will not run on the same terminal instance 
+
+echo "installing npm and all its dependencies"
+npm install
+
+current_dir=$PWD
+echo "Setup script started will resume to ${current_dir} after completion"
+cd node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/
+sudo chown root:root chrome_sandbox
+sudo chmod 4755 chrome_sandbox
+sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
+echo "Setup is completed"
+
+cd $current_dir
