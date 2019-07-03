5687949d1ea6d847cf547b9f41d9bf11384d5a25
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-03 15:17:24 -0400

    Update setup-sandbox.sh

--- setup-sandbox.sh
+++ setup-sandbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/
-sudo chown root:root chrome_sandbox
-sudo chmod 4755 chrome_sandbox
-sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
-export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
+#sudo chown root:root chrome_sandbox
+#sudo chmod 4755 chrome_sandbox
+#sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+#export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
