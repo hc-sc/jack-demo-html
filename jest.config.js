4bceeae1f3ce0cfd8953561f4eef7c881aba2d76
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-03 13:51:59 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
       'displayName': 'unit test',
       'testEnvironment': 'jsdom',
       'testRegex':[
-          '/src/test/unit.test.js',
+          '/src/test/unit-1.test.js',
           '/src/test/unit-2.test.js',
           '/src/test/unit-3.test.js',
           '/src/test/unit-4.test.js'
