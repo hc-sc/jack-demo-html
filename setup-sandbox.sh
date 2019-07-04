af43e5818a567926e3a0fa551a7e4b0ef6896232
bitopibhowmik <bitopi.bhowmik@canada.ca>
2019-07-04 12:24:20 +0000

    update setup

--- setup-sandbox.sh
+++ setup-sandbox.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 current_dir=$PWD
 echo "Setup script started will resume to ${current_dir} after completion"
-
-cd node_modules/puppeteer/.local-chromium/linux-*/chrome-linux
+cd
+cd jack-demo-html/node_modules/puppeteer/.local-chromium/linux-*/chrome-linux
 sudo chown root:root chrome_sandbox
 sudo chmod 4755 chrome_sandbox
 sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
-
 echo "Setup is completed"
 cd $current_dir
