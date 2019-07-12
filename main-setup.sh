204fa86fdcdb208abd49cd8123bfa6228c7d129e
MohamedRadwan <mohamedhisham13@gmail.com>
2019-07-12 08:17:00 -0400

    created one script to setup everyhting

--- main-setup.sh
+++ main-setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
+current_dir=$PWD
+echo "Setup script started will resume to ${current_dir} after completion"
 echo "installing npm and all its dependencies"
 npm install
-echo "calling other scripts"
-. setup-sandbox.sh
+
+echo "installing UI testing sandbox"
+cd node_modules/puppeteer/.local-chromium/linux-*/chrome-linux
+sudo chown root:root chrome_sandbox
+sudo chmod 4755 chrome_sandbox
+sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
+echo "Setup is completed"
+cd $current_dir
