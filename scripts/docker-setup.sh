ec474150fbc4541759f88e8927acc6f5b190cc04
MohamedRadwan <mohamedhisham13@gmail.com>
2019-07-16 09:33:06 -0400

    added error files for Ha proxy, confiugred Haproxy

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
- docker create --name haproxy-open \
+ docker create --name proxy \
     -v /root/haproxy/cfg-file/:/usr/local/etc/haproxy:ro \
 	-v /root/haproxy/error/:/etc/haproxy/errors/ \
+	-v /certs:/certs:ro \
+	-p 22:22 \
     -p 80:80 \
 	-p 443:443 \
     haproxy:1.7
@@ -17,5 +19,19 @@ docker run --name app-error \
  	-p 82:80 \
 	-d nginx
 
+sudo docker run -it --rm \
+-v /srv/letsencrypt/etc:/etc/letsencrypt \
+-v /srv/letsencrypt/var:/var/lib/letsencrypt \
+-p 8888:8888 \
+certbot/certbot \
+certonly --webroot \
+--register-unsafely-without-email --agree-tos \
+--webroot-path=/data/letsencrypt \
+--staging \
+-d majic-student.canadacentral.cloudapp.azure.com -d www.majic-student.canadacentral.cloudapp.azure.com
+
+
+
 docker network connect appnet haproxy-open
 docker network connect appnet nginx-app
+docker network connect appnet nginx-error
