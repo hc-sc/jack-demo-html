c4ad558329a7d1398ef1f2b4770f350bd440779b
MohamedRadwan <mohamedhisham13@gmail.com>
2019-07-17 08:31:24 -0400

    cocnfigured haproxy

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -3,12 +3,10 @@
  docker create --name proxy \
 	-v /home/mradwan/jack-demo-html/haProxy/config/ha_config.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
 	-v /home/mradwan/jack-demo-html/haProxy/error/:/etc/haproxy/errors/ \
-	-v /certs:/etc/haproxy/certs:ro \
     -p 80:80 \
-	-p 443:443 \
     haproxy:1.7
 
-docker run --name app-htm \
+docker run --name app-html \
  	-p 81:80 \
 	-v /home/mradwan/jack-demo-html/dist:/usr/share/nginx/html/html \
 	-v /home/mradwan/jack-demo-html/theme:/usr/share/nginx/html/html/theme \
@@ -18,20 +16,13 @@ docker run --name app-menu \
  	-p 82:80 \
 	-v /home/mradwan/jack-demo-html/menu:/usr/share/nginx/html \
 	-d nginx
-#ssl
-sudo docker run -it --rm \
--v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
--v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
--v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
--v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
-certbot/certbot \
-certonly --webroot \
---register-unsafely-without-email --agree-tos \
---webroot-path=/data/letsencrypt \
---staging \
--d majic-student.canadacentral.cloudapp.azure.com
 
+	sudo certbot certonly --standalone -d majic-student.canadacentral.cloudapp.azure.com \
+    --non-interactive --agree-tos --email mohamed.radwan@canada.ca \
+    --http-01-port=8888
 
-docker network connect appnet haproxy-open
-docker network connect appnet nginx-app
-docker network connect appnet nginx-error
+sudo certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d majic-student.canadacentral.cloudapp.azure.com
+
+docker network connect appnet proxy
+docker network connect appnet app-menu
+docker network connect appnet app-html
