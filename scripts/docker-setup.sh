dd3ca754d49a20f545ad4282ea7c67c1fd0aa254
MohamedRadwan <mohamedhisham13@gmail.com>
2019-07-18 07:32:45 -0400

    optimized script

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
  docker create --name proxy \
-	-v /home/mradwan/jack-demo-html/haProxy/config/ha_config.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
-	-v /home/mradwan/jack-demo-html/haProxy/error/:/etc/haproxy/errors/ \
+	-v /home/mradwan/jack-demo-html/proxy/haProxy/config/ha_config.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+	-v /home/mradwan/jack-demo-html/proxy/haProxy/error/:/etc/haproxy/errors/ \
     -p 80:80 \
     haproxy:1.7
 
 docker run --name app-html \
  	-p 81:80 \
-	-v /home/mradwan/jack-demo-html/dist:/usr/share/nginx/html/html \
-	-v /home/mradwan/jack-demo-html/theme:/usr/share/nginx/html/html/theme \
+	-v /home/mradwan/jack-demo-html/html/dist:/usr/share/nginx/html/html \
+	-v /home/mradwan/jack-demo-html/html/theme:/usr/share/nginx/html/html/theme \
 	-d nginx
 
 docker run --name app-menu \
  	-p 82:80 \
-	-v /home/mradwan/jack-demo-html/menu:/usr/share/nginx/html \
+	-v /home/mradwan/jack-demo-html/html/menu:/usr/share/nginx/html \
 	-d nginx
 
-	sudo certbot certonly --standalone -d majic-student.canadacentral.cloudapp.azure.com \
-    --non-interactive --agree-tos --email mohamed.radwan@canada.ca \
-    --http-01-port=8888
 
-sudo certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d majic-student.canadacentral.cloudapp.azure.com
+docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:7.2-cli php your-script.php
+
+
+certbot certonly --standalone --http-01-port 80 -d majic-student.canadacentral.cloudapp.azure.com
+
+# create a subnet and add them to the subnet
+docker network create \
+    --subnet=172.1.0.0/16 \
+    appnet
+
 
 docker network connect appnet proxy
 docker network connect appnet app-menu
